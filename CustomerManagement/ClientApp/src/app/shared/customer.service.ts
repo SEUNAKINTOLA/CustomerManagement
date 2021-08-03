fbf41eccd82292a087a75702f0cf971f040c04f4
Oluwaseun Akintola <akinsehun@gmail.com>
2021-08-03 08:58:32 +0100

    http

--- CustomerManagement/ClientApp/src/app/shared/customer.service.ts
+++ CustomerManagement/ClientApp/src/app/shared/customer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from "./http";
 import { Guid } from "guid-typescript";
 import { Observable } from 'rxjs';
 import { BehaviorSubject } from 'rxjs';
